69314a144c9745aee88f451298245750cf6e61e3
Anoop Moothedath <anoop98moothedath@gmail.com>
2020-04-03 19:20:57 +0530

    Update BinarySearchTree.c

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-//#include "InsertAndTraversals.c"
 
 struct node { 
   int data; 
@@ -8,13 +7,7 @@ struct node {
 } ;
 
 typedef struct node *BSTNODE;
-/*
-BSTNODE newNodeInBST(int);
-void inorderInBST(BSTNODE);
-void preorderInBST(BSTNODE);
-void postorderInBST(BSTNODE);
-BSTNODE insertNodeInBST(BSTNODE, int);
-*/
+
 BSTNODE newNodeInBST(int item) {
   BSTNODE temp = (BSTNODE)malloc(sizeof(struct node)); 
   temp->data = item;
@@ -65,6 +58,41 @@ BSTNODE insertNodeInBST(BSTNODE node, int ele) {
   return node;
 }
 
+BSTNODE deleteNodeInBST(BSTNODE root, int ele){
+	if(root = NULL){
+		printf("Cannot find %d in the binary search tree.\n",ele);
+		return root;
+	}
+	
+	if(ele < root->data){
+		root->left = deleteNodeInBST(root->left, ele);
+	}
+	else if(ele > root->data){
+		root->right = deleteNodeInBST(root->right, ele);
+	}
+	else if(ele == root->data){
+		if(root->left == NULL){
+			BSTNODE temp = root->right;
+			printf("Deleted %d from binary search tree.\n", ele);
+			free(root);
+			return temp;
+		}
+		
+		else if(root->right == NULL){
+			BSTNODE temp = root->left;
+			printf("Deleted %d from binary search tree.\n", ele);
+			free(root);
+			return temp;
+		}
+		
+		BSTNODE temp = root->left;
+		root = temp->data;
+		temp->data = ele;
+		root->right = deleteNodeInBST(root->right, ele);
+	}
+	return root;
+}
+
 BSTNODE searchNodeInBST(BSTNODE root, int ele){
 	if(root == NULL || root->data == ele)
 		return root;
@@ -78,7 +106,7 @@ int main(){
   int x, op;
   BSTNODE root = NULL;
   while(1) {
-    printf("1.Insert 2.Inorder Traversal 3.Preorder Traversal 4.Postorder Traversal 5.Search 9.Exit\n"); 
+    printf("1.Insert 2.Inorder Traversal 3.Preorder Traversal 4.Postorder Traversal 5.Delete a Node 6.Search 9.Exit\n"); 
     printf("Enter your option :");
     scanf("%d", &op);
     switch(op) {		    	
@@ -113,7 +141,12 @@ int main(){
 		    printf("\n");
 			}
 		  break;
-		case 5: 
+		case 5:
+			printf("Enter an element to be deleted : ");
+			scanf("%d",&x);
+			root = deleteNodeInBST(root,x);
+			break;			  
+		case 6: 
 			printf("Enter an element to be searched : ");
 			scanf("%d",&x);
 			if(searchNodeInBST(root, x) == NULL)
